832435f1459c498989d53b0f607dad03d3147db8
Jacob Bartel <bartel@cs.unc.edu>
2013-04-12 11:13:59 -0400

    Removed extraneous VirtualGraphic class

--- Virtual Toolkit/src/bus/uigen/widgets/graphics/VirtualGraphic.java
+++
@@ -1,9 +0,0 @@
-package bus.uigen.widgets.graphics;
-
-//import java.awt.Graphics;
-
-public abstract class VirtualGraphic /* extends Graphics */{
-
-	public abstract void addDrawnObject(VirtualGraphicObject o);
-
-}
