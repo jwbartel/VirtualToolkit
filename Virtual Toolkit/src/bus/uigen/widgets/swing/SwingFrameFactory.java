2a47624b911005bac3aea30a561d4b9d60105a90
pdewan <dewan@cs.unc.edu>
2015-11-21 18:08:31 -0500

    Factory methods in factory classes for label,textfield, panel and frame

--- Virtual Toolkit/src/bus/uigen/widgets/swing/SwingFrameFactory.java
+++ Virtual Toolkit/src/bus/uigen/widgets/swing/SwingFrameFactory.java
@@ -6,8 +6,15 @@ import bus.uigen.widgets.FrameFactory;
 import bus.uigen.widgets.VirtualFrame;
 
 public class SwingFrameFactory implements FrameFactory {
+	protected JFrame createJFrame() {
+		return new JFrame();
+	}
+	protected JFrame createJFrame(String aTitle) {
+		return new JFrame(aTitle);
+	}
 	public VirtualFrame createFrame() {
-		JFrame frame = new JFrame();
+//		JFrame frame = new JFrame();
+		JFrame frame = createJFrame();
 		// panel.setBackground(Color.white);
 		VirtualFrame toReturn = SwingFrame.virtualFrame(frame);
 //		toReturn.init();
@@ -16,7 +23,9 @@ public class SwingFrameFactory implements FrameFactory {
 	}
 
 	public VirtualFrame createFrame(String title) {
-		JFrame frame = new JFrame(title);
+//		JFrame frame = new JFrame(title);
+		JFrame frame = createJFrame(title);
+
 		// panel.setBackground(Color.white);
 		VirtualFrame toReturn = SwingFrame.virtualFrame(frame);
 //		toReturn.init();
